3711f49e530dc0a4175759a9115d83c9ac96f5a7
Luis Carranza <carranlu@gmail.com>
2021-03-25 19:05:37 -0600

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Luis Javier Carranza on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
